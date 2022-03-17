bea4ebe6f311c67e7bec183adf199ef92bd08869
matko997 <matko.dugandzic@fsre.sum.ba>
2022-03-17 13:33:22 +0100

    added setting endpoint for GET and PATCH

--- src/main/java/com/internship/ratingbackend/model/Setting.java
+++ src/main/java/com/internship/ratingbackend/model/Setting.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
 
 @Entity
@@ -13,6 +15,18 @@ import javax.persistence.*;
 @Setter
 @Table(name = "setting")
 public class Setting {
+    private static final List<Integer> ALLOWED_EMOTION_NUMBER_VALUES = List.of(3, 4, 5);
+    private static final Integer DEFAULT_EMOTION_NUMBER_VALUES = 3;
+
+    private static final Integer MIN_MESSAGE_LENGTH_VALUE = 3;
+    private static final Integer MAX_MESSAGE_LENGTH_VALUE = 128;
+    private static final String DEFAULT_MESSAGE_VALUE = "Thank you for rating";
+
+    private static final Integer MIN_MESSAGE_TIMEOUT_VALUE = 0;
+    private static final Integer MAX_MESSAGE_TIMEOUT_VALUE = 10;
+    private static final Integer DEFAULT_MESSAGE_TIMEOUT_VALUE = 5;
+
+
     @Id
     @GeneratedValue(
             strategy = GenerationType.IDENTITY
@@ -21,7 +35,7 @@ public class Setting {
     private Integer id;
 
 
-    @Column(name = "emotion_number",nullable = false,columnDefinition = "SMALLINT")
+    @Column(name = "emotion_number", nullable = false, columnDefinition = "SMALLINT")
     private Integer emotionNumber;
 
 
@@ -33,8 +47,21 @@ public class Setting {
 
 
     public Setting(Integer emotionNumber, String message, Integer messageTimeout) {
-        this.emotionNumber = emotionNumber;
-        this.message = message;
-        this.messageTimeout = messageTimeout;
+
+        this.emotionNumber = Objects.requireNonNullElse(emotionNumber, DEFAULT_EMOTION_NUMBER_VALUES);
+        this.message = Objects.requireNonNullElse(message, DEFAULT_MESSAGE_VALUE);
+        this.messageTimeout = Objects.requireNonNullElse(messageTimeout, DEFAULT_MESSAGE_TIMEOUT_VALUE);
+
+        try {
+            if ((!ALLOWED_EMOTION_NUMBER_VALUES.contains(emotionNumber)) ||
+                    (message.length() < MIN_MESSAGE_LENGTH_VALUE || message.length() > MAX_MESSAGE_LENGTH_VALUE) ||
+                    (messageTimeout < MIN_MESSAGE_TIMEOUT_VALUE || messageTimeout > MAX_MESSAGE_TIMEOUT_VALUE)) {
+                throw new RuntimeException();
+            }
+
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
