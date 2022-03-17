88e54f6fbac90651f6cd28007a5c57c52bb39a70
Antonio <AntonioPavkovic>
2022-03-17 15:26:31 +0100

    updated rating route

--- src/main/java/com/internship/ratingbackend/controller/RatingController.java
+++ src/main/java/com/internship/ratingbackend/controller/RatingController.java
@@ -3,12 +3,14 @@ package com.internship.ratingbackend.controller;
 import com.internship.ratingbackend.model.Rating;
 import com.internship.ratingbackend.service.RatingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.rmi.ServerException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -25,10 +27,10 @@ public class RatingController {
 
     // method/route that gets ratings between two dates
     @GetMapping("/between")
-    public List<Rating> getRatingByCreatedAtBetween (/*@RequestParam(name = "created_at") LocalDateTime fromDate,
-                                                @RequestParam LocalDateTime toDate*/) {
+    public List<Rating> getRatingByCreatedAtBetween (@RequestParam(name = "created_at") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime fromDate,
+                                                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime toDate) {
 
-        return ratingService.getRatingByCreatedAtBetween(/*fromDate, toDate*/);
+        return ratingService.getRatingByCreatedAtBetween(fromDate, toDate);
 
     }
 
