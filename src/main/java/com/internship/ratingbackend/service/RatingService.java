1551d5544c3ddfc8e04aa5aee85bfa071a052c0d
Antonio <AntonioPavkovic>
2022-04-04 14:40:43 +0200

    update to slack service

--- src/main/java/com/internship/ratingbackend/service/RatingService.java
+++ src/main/java/com/internship/ratingbackend/service/RatingService.java
@@ -1,9 +1,15 @@
 package com.internship.ratingbackend.service;
 
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.webhook.WebhookResponse;
+import com.internship.ratingbackend.config.AppProperties;
 import com.internship.ratingbackend.dto.rating.RatingResponse;
 import com.internship.ratingbackend.model.Rating;
 import com.internship.ratingbackend.repository.RatingRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -13,11 +19,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RatingService {
 
     private final RatingRepository ratingRepository;
     private final Integer ALLOWED_RANGE_DATE_DAYS = 30;
-
+    private final AppProperties appProperties;
+    private final Slack slack;
 
     public List<Rating> getRatingByCreatedAtBetween(LocalDateTime fromDate, LocalDateTime toDate) {
 
@@ -53,5 +61,31 @@ public class RatingService {
     public RatingResponse buildSingle(Rating rating)
     {
         return new RatingResponse(rating);
+    }
+
+    @Scheduled(cron = "*/86400 * * * * *")
+    @SneakyThrows
+    public void sendSlackReport() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime morningDateTime = LocalDateTime.now();
+
+        morningDateTime = morningDateTime.minusHours(localDateTime.getHour())
+                .minusMinutes(localDateTime.getMinute())
+                .minusSeconds(localDateTime.getSecond());
+
+        List<Rating> list = ratingRepository.getRatingByCreatedAtBetween(morningDateTime, localDateTime);
+
+
+        if ((long) list.size() < 10) {
+            log.info("Sending scheduled slack report! Ratings are lower then 10");
+
+            String webhookUrl = appProperties.getSlackReportLink();
+            String payload = "{\"text\":\"There has been less than 10 ratings today!\"}";
+
+            WebhookResponse response = slack.send(webhookUrl, payload);
+
+            log.info(response.toString());
+
+        }
     }
 }
