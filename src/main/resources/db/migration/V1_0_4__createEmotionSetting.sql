4626bc7737d2b53afdbc6471bef3cf997099671e
matko997 <matko.dugandzic@fsre.sum.ba>
2022-04-13 09:22:22 +0200

    changed order of emotions

--- src/main/resources/db/migration/V1_0_4__createEmotionSetting.sql
+++ src/main/resources/db/migration/V1_0_4__createEmotionSetting.sql
@@ -5,15 +5,15 @@ CREATE TABLE IF NOT EXISTS emotion_setting (
     constraint emotion_id_emotion_setting_constraints_emotion_id_emotion foreign key(emotion_id) references emotion(id)
     )ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
-INSERT INTO emotion_setting (emotion_id,emotion_value) VALUES  (1, 3),
+INSERT INTO emotion_setting (emotion_id,emotion_value) VALUES  (5, 3),
                                                                (3, 3),
-                                                               (5, 3),
-                                                               (1, 4),
-                                                               (2, 4),
-                                                               (4, 4),
+                                                               (1, 3),
                                                                (5, 4),
-                                                               (1, 5),
-                                                               (2, 5),
-                                                               (3, 5),
+                                                               (4, 4),
+                                                               (2, 4),
+                                                               (1, 4),
+                                                               (5, 5),
                                                                (4, 5),
-                                                               (5, 5);
+                                                               (3, 5),
+                                                               (2, 5),
+                                                               (1, 5);
